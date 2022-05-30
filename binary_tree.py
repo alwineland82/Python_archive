5c806fd579f9644675e431eaf4baa9d793bfdd83
alwineland82 <103864391+alwineland82@users.noreply.github.com>
2022-05-30 11:28:11 +1100

    Create binary_tree.py

---
+++ binary_tree.py
@@ -0,0 +1,25 @@
+class Node:
+    def __init__(self, L, R, n):
+        self.left = L
+        self.right = R
+        self.value = n
+        
+        
+
+def tree_by_levels(node):
+    lst = [node]
+    if node is None:
+        return []
+    
+    else:
+        for i in lst:
+            if i.__dict__['left'] != None:
+                lst.append(i.__dict__['left'])
+            if i.__dict__['right'] != None:
+                lst.append(i.__dict__['right'])
+    
+    
+    print(list(map(lambda x: x.value, lst)))
+    
+
+print(tree_by_levels((Node(Node(None, Node(None, None, 4), 2), Node(Node(None, None, 5), Node(None, None, 6), 3), 1))))
